ef518ef8068fa890c29ea4a444d2260a4080a8a7
Chris Yeoward <cyeoward@Chris-Yeoward.local>
2016-07-08 17:14:22 +0100

    CY AM LF | Finish first question with or without JS.
    We've switched to 'html centric', ie the questions, and answers are stored in the DOM and just updated, hidden and shown by javascript.

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,4 +1,37 @@
-function nameResponse() {
-  var nameAnswer = document.getElementById("name-answer").value;
-  document.getElementById("name-response").innerHTML = "Hi" + nameAnswer + " nice to meet you.";
+var answerArray = [];
+var progress = null;
+var selectedQuestion = null;
+
+function updateQuestionWithResponse(questionContainer, answer){
+  var responseContainer = questionContainer.getElementsByClassName('response-container')[0];
+
+  var questionQuestionContainer = questionContainer.getElementsByClassName("question-container")[0]
+    .classList.add("hidden");
+  var inputContainer = questionContainer.getElementsByClassName("input-container")[0]
+      .classList.add("hidden");
+
+  var answerThing =responseContainer.getElementsByClassName("response-text")[0].innerHTML = answer;
+  responseContainer.classList.remove("hidden");
+}
+
+function updateResponse() {
+
+}
+
+function answerSubmit(questionNumber) {
+  var questionContainerId = "question-" + questionNumber;
+  var questionNode = document.getElementById(questionContainerId);
+
+  var answer = questionNode.getElementsByClassName("answer")[0].value;
+  answerArray[questionNumber-1] = answer;
+  console.log(answerArray[questionNumber]);
+
+  updateQuestionWithResponse(questionNode, answer);
+  //update currentQuestion with the answer just entered
+  //Hi name, nice to meet you
+
+  //show the next question?
+  //(where do you live?)
+
+  //update progress variable
 }
