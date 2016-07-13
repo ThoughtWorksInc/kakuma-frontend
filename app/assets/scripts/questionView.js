eec6a8e4845c81a2df49b190ac9bb7a9c79a4008
Lucy Fang <lucyfang2@gmail.com>
2016-07-13 14:30:31 +0100

    AM LF | moved questionView into a separate file

---
+++ app/assets/scripts/questionView.js
@@ -0,0 +1,40 @@
+function getQuestionNode (questionNumber) {
+  return document.getElementById("question-" + questionNumber);
+}
+
+function getQuestionView(questionNumber) {
+  var questionView = {
+    questionNode: getQuestionNode(questionNumber),
+    showTextbox: function () {
+      this.questionNode.getElementsByClassName("input-container")[0]
+        .classList
+        .remove("hidden");
+      return this;
+    },
+    getAnswer: function() {
+      return this.questionNode.getElementsByClassName("answer")[0].value;
+    },
+    displayResponseWith: function(answer) {
+      this.questionNode.getElementsByClassName("response-text")[0].innerHTML = answer;
+      this.questionNode.getElementsByClassName("response-container")[0].classList.remove("hidden");
+      return this;
+    },
+    hideQuestionText:function() {
+      this.questionNode.getElementsByClassName("question-container")[0]
+        .classList.add("hidden");
+      return this;
+    },
+    show: function() {
+      this.questionNode.classList.remove("hidden");
+      this.showTextbox();
+      return this;
+    },
+    highlightQuestionForEdit: function() {
+      this.questionNode.classList.add("edit-highlight");
+    },
+    removeEditHighlight: function() {
+      this.questionNode.classList.remove("edit-highlight");
+    }
+  };
+  return questionView;
+}
