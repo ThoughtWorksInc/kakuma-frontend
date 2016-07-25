264b5259a1818ef37920f371a945d4d1908783ef
andrew-m <andrew.maddison@thoughtworks.com>
2016-07-25 16:11:47 +0100

    CY AM | Adding preprocess script called from go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,3 @@
-sass --update app/assets/stylesheets:public/stylesheets
-
-slimrb app/views/index.slim > public/views/index.html
-
-cp -a app/assets/scripts/. public/scripts
-
-cp -a app/assets/images/. public/images
+./preprocess.sh
 
 rackup
