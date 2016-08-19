db9d776c704bbac5405ab31c5a73c6c23b5e9e36
Lucy Fang <lucyfang2@gmail.com>
2016-08-19 14:51:53 +0100

    LF | testing config for connection with aws rds

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -3,12 +3,12 @@ require './app/src/model/Person'
 
 # DataMapper.setup(:default, 'postgres://kakuma_user:sausage@localhost/kakuma_dev')
 
-db_name = ENV['RDS_DB_NAME']
-rds_password = ENV['RDS_PASSWORD']
-rds_port = ENV['RDS_PORT']
+# db_name = ENV['RDS_DB_NAME']
+# rds_password = ENV['RDS_PASSWORD']
+# rds_port = ENV['RDS_PORT']
 
 
-DataMapper.setup(:default, "postgres://#{db_name}:#{rds_password}@kakumadev-db.c9eswbufhaxy.eu-west-1.rds.amazonaws.com:#{rds_port}")
+DataMapper.setup(:default, "postgres://kakumadev:sausages@kakumadev-db.c9eswbufhaxy.eu-west-1.rds.amazonaws.com:5432")
 
 DataMapper::Property::String.length(255)
 
