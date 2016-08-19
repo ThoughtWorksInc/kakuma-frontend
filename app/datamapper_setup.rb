c3e1de53af3ce70cc48c20a768db475dd990da3c
Lucy Fang <lucyfang2@gmail.com>
2016-08-19 14:31:14 +0100

    LF | Added missing bracket in datamapper setup

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -3,11 +3,11 @@ require './app/src/model/Person'
 
 # DataMapper.setup(:default, 'postgres://kakuma_user:sausage@localhost/kakuma_dev')
 
-DataMapper.setup(:default, "postgres://ENV['RDS_DB_NAME']:ENV['RDS_PASSWORD']@kakumadev-db.c9eswbufhaxy.eu-west-1.rds.amazonaws.com:ENV['RDS_PORT']"
+DataMapper.setup(:default, "postgres://ENV['RDS_DB_NAME']:ENV['RDS_PASSWORD']@kakumadev-db.c9eswbufhaxy.eu-west-1.rds.amazonaws.com:ENV['RDS_PORT']")
 
 DataMapper::Property::String.length(255)
 
 DataMapper.finalize
 
 DataMapper.auto_migrate!
-git 
+git
