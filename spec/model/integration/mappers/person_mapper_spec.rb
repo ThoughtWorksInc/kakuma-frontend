6f0c4f16475da62985f475ae7f259fd6f263131a
Lucy Fang <lucyfang2@gmail.com>
2016-08-23 15:57:14 +0100

    LF MB | Added test for retrieving json data and refactored tests

--- spec/model/integration/mappers/person_mapper_spec.rb
+++ spec/model/integration/mappers/person_mapper_spec.rb
@@ -11,12 +11,22 @@ describe Person do
     expect(people[0].details).to eq data
   end
 
-  it "should save the full JSON map string to the database" do
-    details = {"name":"lucy","gender":"female","age":"23","village":"melbourne","country":"australia","phoneNumber":"+61422885870","nameToFind":"chris","relation":"brother","villageToFind":"london","countryToFind":"UK"}
-    lucy = Person.new(:details => details)
-    lucy.save
-    people = Person.all
-    expect(people[0].details).to eq details
+  context "saving and retrieving JSON data" do
+
+    before(:each) do
+      @details = {"name":"lucy","gender":"female","age":"23","village":"melbourne","country":"australia","phoneNumber":"+61422885870","nameToFind":"chris","relation":"brother","villageToFind":"london","countryToFind":"UK"}
+      lucy = Person.new(:details => @details)
+      lucy.save
+      @people = Person.all
+    end
+
+    it "should save the full JSON map string to the database" do
+      expect(@people[0].details).to eq @details
+    end
+
+    it "should retrieve name from a record in the database" do
+      expect(@people[0].details[:name]).to eq "lucy"
+    end
   end
 
 end
