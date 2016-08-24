0e0de82f7e67021d1afbe3ff7ddec8142436d6f3
Lucy Fang <lucyfang2@gmail.com>
2016-08-24 11:52:30 +0100

    MB MR LF | printing out RDS environment variables

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -13,9 +13,17 @@ host = ENV['RDS_HOSTNAME'] || "localhost"
 dbName = ENV['RDS_DB_NAME'] || "kakuma_dev"
 userName = ENV['RDS_USERNAME'] || "kakuma_user"
 password = ENV['RDS_PASSWORD'] || "sausage"
+# port = ENV['RDS_PORT'] || "5432"
 url = "postgres://#{userName}:#{password}@#{host}/#{dbName}"
+puts url
+# RDS_HOSTNAME – The hostname of the DB instance.
+# RDS_DB_NAME – The database name, ebdb.
+# RDS_USERNAME – The username that you configured for your database.
+# RDS_PASSWORD – The password that you configured for your database.
+# RDS_PORT – The port on which the DB instance accepts connections. The default value varies between DB engines.
+# postgres://kakumaDbUser:bRcOtLvmFP6g@aa1ry1o23s2mm7y.c9eswbufhaxy.eu-west-1.rds.amazonaws.com/ebdb
 
-DataMapper.setup(:default, url)
+DataMapper.setup(:default, ENV['RDS_DB_URL'] || "postgres://kakuma_user:sausage@localhost/kakuma_dev")
 
 DataMapper::Property::String.length(255)
 
