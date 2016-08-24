cd5ed4d2ca0b72c7fb7efa80c63693d2f5cb0eec
Lucy Fang <lucyfang2@gmail.com>
2016-08-24 11:35:29 +0100

    MB MR LF | constructed db url from aws environment variables

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -2,8 +2,13 @@ require 'data_mapper'
 require './app/src/model/Person'
 
 # DataMapper::Logger.new($stdout, :debug)
+host = ENV['RDS_HOSTNAME'] || "localhost"
+dbName = ENV['RDS_DB_NAME'] || "kakuma_dev"
+userName = ENV['RDS_USERNAME'] || "kakuma_user"
+password = ENV['RDS_PASSWORD'] || "sausage"
+url = "postgres://#{userName}:#{password}@#{host}/#{dbName}"
 
-DataMapper.setup(:default, ENV['RDS_DB_URL'] || "postgres://kakuma_user:sausage@localhost/kakuma_dev")
+DataMapper.setup(:default, url)
 
 DataMapper::Property::String.length(255)
 
