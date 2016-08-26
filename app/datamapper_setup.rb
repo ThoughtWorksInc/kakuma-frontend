b6af0dc09db965e368d59143dd32cbde0dd05292
Lucy Fang <lucyfang2@gmail.com>
2016-08-26 11:09:03 +0100

    MB MR LF | printing out environment vars

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -1,11 +1,18 @@
 require 'data_mapper'
 require './app/src/model/Person'
 
+
+puts "****************TGIF************************"
+puts ENV['RDS_HOSTNAME']
+
 host = ENV['RDS_HOSTNAME'] || "localhost"
 dbName = ENV['RDS_DB_NAME'] || "kakuma_dev"
 userName = ENV['RDS_USERNAME'] || "kakuma_user"
 password = ENV['RDS_PASSWORD'] || "sausage"
 url = "postgres://#{userName}:#{password}@#{host}/#{dbName}"
+
+puts "****************TGIF2************************"
+puts url
 
 DataMapper.setup(:default, url)
 
